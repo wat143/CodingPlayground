1e0a1332188d6fcd537b61791c4fc555406be66e
Wataru Mizuno <watarumizuno1128@gmail.com>
2021-12-10 10:50:32 +0900

    WebGL: Fill color in 2D triangle

--- WebGL/2DTriangle.js
+++ WebGL/2DTriangle.js
@@ -1,149 +1,324 @@
-/*------Render CUBE Example------*/
-var points = [];
-var colors = [];
-var numVertices = 36;
+// ------------------------------------------------------------------------------------------------
+// minMatrix.js
+// version 0.0.1
+// Copyright (c) doxas
+// ------------------------------------------------------------------------------------------------
+
+function matIV(){
+	this.create = function(){
+		return new Float32Array(16);
+	};
+	this.identity = function(dest){
+		dest[0]  = 1; dest[1]  = 0; dest[2]  = 0; dest[3]  = 0;
+		dest[4]  = 0; dest[5]  = 1; dest[6]  = 0; dest[7]  = 0;
+		dest[8]  = 0; dest[9]  = 0; dest[10] = 1; dest[11] = 0;
+		dest[12] = 0; dest[13] = 0; dest[14] = 0; dest[15] = 1;
+		return dest;
+	};
+	this.multiply = function(mat1, mat2, dest){
+		var a = mat1[0],  b = mat1[1],  c = mat1[2],  d = mat1[3],
+			e = mat1[4],  f = mat1[5],  g = mat1[6],  h = mat1[7],
+			i = mat1[8],  j = mat1[9],  k = mat1[10], l = mat1[11],
+			m = mat1[12], n = mat1[13], o = mat1[14], p = mat1[15],
+			A = mat2[0],  B = mat2[1],  C = mat2[2],  D = mat2[3],
+			E = mat2[4],  F = mat2[5],  G = mat2[6],  H = mat2[7],
+			I = mat2[8],  J = mat2[9],  K = mat2[10], L = mat2[11],
+			M = mat2[12], N = mat2[13], O = mat2[14], P = mat2[15];
+		dest[0] = A * a + B * e + C * i + D * m;
+		dest[1] = A * b + B * f + C * j + D * n;
+		dest[2] = A * c + B * g + C * k + D * o;
+		dest[3] = A * d + B * h + C * l + D * p;
+		dest[4] = E * a + F * e + G * i + H * m;
+		dest[5] = E * b + F * f + G * j + H * n;
+		dest[6] = E * c + F * g + G * k + H * o;
+		dest[7] = E * d + F * h + G * l + H * p;
+		dest[8] = I * a + J * e + K * i + L * m;
+		dest[9] = I * b + J * f + K * j + L * n;
+		dest[10] = I * c + J * g + K * k + L * o;
+		dest[11] = I * d + J * h + K * l + L * p;
+		dest[12] = M * a + N * e + O * i + P * m;
+		dest[13] = M * b + N * f + O * j + P * n;
+		dest[14] = M * c + N * g + O * k + P * o;
+		dest[15] = M * d + N * h + O * l + P * p;
+		return dest;
+	};
+	this.scale = function(mat, vec, dest){
+		dest[0]  = mat[0]  * vec[0];
+		dest[1]  = mat[1]  * vec[0];
+		dest[2]  = mat[2]  * vec[0];
+		dest[3]  = mat[3]  * vec[0];
+		dest[4]  = mat[4]  * vec[1];
+		dest[5]  = mat[5]  * vec[1];
+		dest[6]  = mat[6]  * vec[1];
+		dest[7]  = mat[7]  * vec[1];
+		dest[8]  = mat[8]  * vec[2];
+		dest[9]  = mat[9]  * vec[2];
+		dest[10] = mat[10] * vec[2];
+		dest[11] = mat[11] * vec[2];
+		dest[12] = mat[12];
+		dest[13] = mat[13];
+		dest[14] = mat[14];
+		dest[15] = mat[15];
+		return dest;
+	};
+	this.translate = function(mat, vec, dest){
+		dest[0] = mat[0]; dest[1] = mat[1]; dest[2]  = mat[2];  dest[3]  = mat[3];
+		dest[4] = mat[4]; dest[5] = mat[5]; dest[6]  = mat[6];  dest[7]  = mat[7];
+		dest[8] = mat[8]; dest[9] = mat[9]; dest[10] = mat[10]; dest[11] = mat[11];
+		dest[12] = mat[0] * vec[0] + mat[4] * vec[1] + mat[8]  * vec[2] + mat[12];
+		dest[13] = mat[1] * vec[0] + mat[5] * vec[1] + mat[9]  * vec[2] + mat[13];
+		dest[14] = mat[2] * vec[0] + mat[6] * vec[1] + mat[10] * vec[2] + mat[14];
+		dest[15] = mat[3] * vec[0] + mat[7] * vec[1] + mat[11] * vec[2] + mat[15];
+		return dest;
+	};
+	this.rotate = function(mat, angle, axis, dest){
+		var sq = Math.sqrt(axis[0] * axis[0] + axis[1] * axis[1] + axis[2] * axis[2]);
+		if(!sq){return null;}
+		var a = axis[0], b = axis[1], c = axis[2];
+		if(sq != 1){sq = 1 / sq; a *= sq; b *= sq; c *= sq;}
+		var d = Math.sin(angle), e = Math.cos(angle), f = 1 - e,
+			g = mat[0],  h = mat[1], i = mat[2],  j = mat[3],
+			k = mat[4],  l = mat[5], m = mat[6],  n = mat[7],
+			o = mat[8],  p = mat[9], q = mat[10], r = mat[11],
+			s = a * a * f + e,
+			t = b * a * f + c * d,
+			u = c * a * f - b * d,
+			v = a * b * f - c * d,
+			w = b * b * f + e,
+			x = c * b * f + a * d,
+			y = a * c * f + b * d,
+			z = b * c * f - a * d,
+			A = c * c * f + e;
+		if(angle){
+			if(mat != dest){
+				dest[12] = mat[12]; dest[13] = mat[13];
+				dest[14] = mat[14]; dest[15] = mat[15];
+			}
+		} else {
+			dest = mat;
+		}
+		dest[0] = g * s + k * t + o * u;
+		dest[1] = h * s + l * t + p * u;
+		dest[2] = i * s + m * t + q * u;
+		dest[3] = j * s + n * t + r * u;
+		dest[4] = g * v + k * w + o * x;
+		dest[5] = h * v + l * w + p * x;
+		dest[6] = i * v + m * w + q * x;
+		dest[7] = j * v + n * w + r * x;
+		dest[8] = g * y + k * z + o * A;
+		dest[9] = h * y + l * z + p * A;
+		dest[10] = i * y + m * z + q * A;
+		dest[11] = j * y + n * z + r * A;
+		return dest;
+	};
+	this.lookAt = function(eye, center, up, dest){
+		var eyeX    = eye[0],    eyeY    = eye[1],    eyeZ    = eye[2],
+			upX     = up[0],     upY     = up[1],     upZ     = up[2],
+			centerX = center[0], centerY = center[1], centerZ = center[2];
+		if(eyeX == centerX && eyeY == centerY && eyeZ == centerZ){return this.identity(dest);}
+		var x0, x1, x2, y0, y1, y2, z0, z1, z2, l;
+		z0 = eyeX - center[0]; z1 = eyeY - center[1]; z2 = eyeZ - center[2];
+		l = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
+		z0 *= l; z1 *= l; z2 *= l;
+		x0 = upY * z2 - upZ * z1;
+		x1 = upZ * z0 - upX * z2;
+		x2 = upX * z1 - upY * z0;
+		l = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
+		if(!l){
+			x0 = 0; x1 = 0; x2 = 0;
+		} else {
+			l = 1 / l;
+			x0 *= l; x1 *= l; x2 *= l;
+		}
+		y0 = z1 * x2 - z2 * x1; y1 = z2 * x0 - z0 * x2; y2 = z0 * x1 - z1 * x0;
+		l = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
+		if(!l){
+			y0 = 0; y1 = 0; y2 = 0;
+		} else {
+			l = 1 / l;
+			y0 *= l; y1 *= l; y2 *= l;
+		}
+		dest[0] = x0; dest[1] = y0; dest[2]  = z0; dest[3]  = 0;
+		dest[4] = x1; dest[5] = y1; dest[6]  = z1; dest[7]  = 0;
+		dest[8] = x2; dest[9] = y2; dest[10] = z2; dest[11] = 0;
+		dest[12] = -(x0 * eyeX + x1 * eyeY + x2 * eyeZ);
+		dest[13] = -(y0 * eyeX + y1 * eyeY + y2 * eyeZ);
+		dest[14] = -(z0 * eyeX + z1 * eyeY + z2 * eyeZ);
+		dest[15] = 1;
+		return dest;
+	};
+	this.perspective = function(fovy, aspect, near, far, dest){
+		var t = near * Math.tan(fovy * Math.PI / 360);
+		var r = t * aspect;
+		var a = r * 2, b = t * 2, c = far - near;
+		dest[0] = near * 2 / a;
+		dest[1] = 0;
+		dest[2] = 0;
+		dest[3] = 0;
+		dest[4] = 0;
+		dest[5] = near * 2 / b;
+		dest[6] = 0;
+		dest[7] = 0;
+		dest[8] = 0;
+		dest[9] = 0;
+		dest[10] = -(far + near) / c;
+		dest[11] = -1;
+		dest[12] = 0;
+		dest[13] = 0;
+		dest[14] = -(far * near * 2) / c;
+		dest[15] = 0;
+		return dest;
+	};
+	this.transpose = function(mat, dest){
+		dest[0]  = mat[0];  dest[1]  = mat[4];
+		dest[2]  = mat[8];  dest[3]  = mat[12];
+		dest[4]  = mat[1];  dest[5]  = mat[5];
+		dest[6]  = mat[9];  dest[7]  = mat[13];
+		dest[8]  = mat[2];  dest[9]  = mat[6];
+		dest[10] = mat[10]; dest[11] = mat[14];
+		dest[12] = mat[3];  dest[13] = mat[7];
+		dest[14] = mat[11]; dest[15] = mat[15];
+		return dest;
+	};
+	this.inverse = function(mat, dest){
+		var a = mat[0],  b = mat[1],  c = mat[2],  d = mat[3],
+			e = mat[4],  f = mat[5],  g = mat[6],  h = mat[7],
+			i = mat[8],  j = mat[9],  k = mat[10], l = mat[11],
+			m = mat[12], n = mat[13], o = mat[14], p = mat[15],
+			q = a * f - b * e, r = a * g - c * e,
+			s = a * h - d * e, t = b * g - c * f,
+			u = b * h - d * f, v = c * h - d * g,
+			w = i * n - j * m, x = i * o - k * m,
+			y = i * p - l * m, z = j * o - k * n,
+			A = j * p - l * n, B = k * p - l * o,
+			ivd = 1 / (q * B - r * A + s * z + t * y - u * x + v * w);
+		dest[0]  = ( f * B - g * A + h * z) * ivd;
+		dest[1]  = (-b * B + c * A - d * z) * ivd;
+		dest[2]  = ( n * v - o * u + p * t) * ivd;
+		dest[3]  = (-j * v + k * u - l * t) * ivd;
+		dest[4]  = (-e * B + g * y - h * x) * ivd;
+		dest[5]  = ( a * B - c * y + d * x) * ivd;
+		dest[6]  = (-m * v + o * s - p * r) * ivd;
+		dest[7]  = ( i * v - k * s + l * r) * ivd;
+		dest[8]  = ( e * A - f * y + h * w) * ivd;
+		dest[9]  = (-a * A + b * y - d * w) * ivd;
+		dest[10] = ( m * u - n * s + p * q) * ivd;
+		dest[11] = (-i * u + j * s - l * q) * ivd;
+		dest[12] = (-e * z + f * x - g * w) * ivd;
+		dest[13] = ( a * z - b * x + c * w) * ivd;
+		dest[14] = (-m * t + n * r - o * q) * ivd;
+		dest[15] = ( i * t - j * r + k * q) * ivd;
+		return dest;
+	};
+}
+//////////////////////////////////////////////////////////////////////////
 
 
-function quad(a, b, c, d)
-{
-    var vertices = [
-        vec4.fromValues( -0.5, -0.5,  0.5, 1.0 ),
-        vec4.fromValues( -0.5,  0.5,  0.5, 1.0 ),
-        vec4.fromValues(  0.5,  0.5,  0.5, 1.0 ),
-        vec4.fromValues(  0.5, -0.5,  0.5, 1.0 ),
-        vec4.fromValues( -0.5, -0.5, -0.5, 1.0 ),
-        vec4.fromValues( -0.5,  0.5, -0.5, 1.0 ),
-        vec4.fromValues(  0.5,  0.5, -0.5, 1.0 ),
-        vec4.fromValues(  0.5, -0.5, -0.5, 1.0 )
-    ];
+// Shader sources 
+vert = 'attribute vec3 position; attribute vec4 color; uniform mat4 mvpMatrix; varying vec4 vColor; void main() { vColor = color; gl_Position = mvpMatrix * vec4(position, 1.0);}'
+frag = 'precision mediump float; varying vec4 vColor; void main() { gl_FragColor = vColor; }'
 
-    var vertexColors = [
-        [ 0.0, 0.0, 0.0, 1.0 ],  // black
-        [ 1.0, 0.0, 0.0, 1.0 ],  // red
-        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
-        [ 0.0, 1.0, 0.0, 1.0 ],  // green
-        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
-        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
-        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
-        [ 1.0, 1.0, 1.0, 1.0 ]   // white
-    ];
-
-    // We need to parition the quad into two triangles in order for
-    // WebGL to be able to render it.  In this case, we create two
-    // triangles from the quad indices
-
-    //vertex color assigned by the index of the vertex
-    var indices = [ a, b, c, a, c, d ];
-
-    for ( var i = 0; i < indices.length; ++i ) {
-        points.push( vertices[indices[i]] );
-        colors.push( vertexColors[indices[i]] );
-
-        // for solid colored faces use
-        //colors.push(vertexColors[a]);
-
-    }
+// Create shader function 
+function createProgram(vs, fs) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+        alert(gl.getProgramInfoLog(program, gl.LINK_STATUS));
+    gl.useProgram(program);
+    return program;    
 }
 
-//constructs quads from two triangles
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+// Create program function 
+function createShader() {
+    var vShader, fShader;
+    vShader = gl.createShader(gl.VERTEX_SHADER);
+    fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vShader, vert);
+    gl.shaderSource(fShader, frag);
+    gl.compileShader(vShader);
+    if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS))
+        alert(gl.getShaderInfoLog(vShader));
+    gl.compileShader(fShader);
+    if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS))
+        alert(gl.getShaderInfoLog(fShader));
+        
+    return createProgram(vShader, fShader);
 }
 
-colorCube();
 
-// Enable depth testing, used for hidden surface removal
-gl.enable(gl.DEPTH_TEST);
-// Near things obscure far things
-gl.depthFunc(gl.LEQUAL);
-//Load shaders and initialize attribute buffers
-var program = initShaders(gl);
-//after gl.useprogram all Uniforms will be set for that program
-//additionally tells WebGL which shader program-pair to use.
-gl.useProgram(program);
-
-
-//creates an empty buffer
-var cBuffer = gl.createBuffer();
-
-//binds the cBuffer in order to tell WebGl in which buffer we want to modify
-gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-
-//store the colors Array in our cBuffer
-//Flatten is a helper function which converts the Javascript array 'colors' into a Float32Array
-//reason is that WebGL buffers can only store strongly typed float arrays.
-gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
-
-//get the location of the attribute 'vColor' from our Vertex Shader
-var vColor = gl.getAttribLocation( program, "vColor" );
-
-//tell WebGL how to retrive the stored data out of our cBuffer
-//in this case 4 values of the buffer for each vertex
-gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-//turns the generic vertex attribute array on at the given index position vColor.
-gl.enableVertexAttribArray( vColor );
-
-var vBuffer = gl.createBuffer();
-gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-
-//get the location of the attribute 'vPosition' from our Vertex Shader
-var vPosition = gl.getAttribLocation( program, "vPosition" );
-//tell WebGL how to retrive the stored data out of our vBuffer
-//in this case 4 values of the buffer for each vertex
-gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-
-//turns the generic vertex attribute array on at the given index position vPosition.
-gl.enableVertexAttribArray( vPosition );
-
-//create our (identity) projection 4x4 matrix
-var projectionMatrix = mat4.create();
-var aspect = canvas.clientWidth / canvas.clientHeight;
-
-//create a perspective matrix and writes the values into the projectionMatrix
-mat4.perspective(projectionMatrix, 45 , aspect, 1, 100);
-
-//find the uniform location of the uniform 'projection' in our vertex shader
-var projectionPosition = gl.getUniformLocation(program, "projection");
-
-//upload the projectionMatrix to the vertex Shader at our position  'projectionPosition'
-gl.uniformMatrix4fv(projectionPosition, false, projectionMatrix);
-
-//create our (identity) view 4x4 matrices
-var viewMatrix = mat4.create();
-
-//this helper function connects the user-interaction with the viewMatrix and manipulates it on render time
-lookAtArcballCamera(viewMatrix, vec3.fromValues(0,0,5), vec3.fromValues(0,0,0), vec3.fromValues(0,1,0));
-
-//find the uniform location of the uniform 'view' in our vertex shader
-var viewPosition = gl.getUniformLocation(program, "view");
-
-//upload the viewMatrix to the vertex Shader at our position 'viewPosition'
-gl.uniformMatrix4fv(viewPosition, false,viewMatrix);
-
-//create our (identity) model 4x4 matrices
-var modelMatrix = mat4.create();
-//find the uniform location of the uniform 'model' in our vertex shader
-var modelPosition = gl.getUniformLocation(program, "model");
-
-//upload the modelMatrix to the vertex Shader at our position 'modelPosition'
-gl.uniformMatrix4fv(modelPosition, false,modelMatrix);
-
-
-//this is the main render function (you'll have to define this function 'render()') which will be automatically executed once for each frame!
-function render(){
-    // Clears the color and depth buffers
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    //update the manipulation on the viewMatrix (send the new viewMatrix to the Vertex Shader)
-    gl.uniformMatrix4fv(viewPosition, false,viewMatrix);
-
-    //start drawing Triangle primitives (the primitive tells WebGL how many vertices to use in order to construct a surface)
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+// Create VBO 
+function createVBO(data) {
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null); // Unbind buffer for next buffer manipulation
+    return vbo;
 }
+
+
+// Clear Context 
+gl.clearColor(1.0, 1.0, 1.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+// Clear DEPTH
+gl.clearDepth(1.0);
+
+// Create program 
+var prg = createShader();
+var attLocation = new Array(2);
+attLocation[0] = gl.getAttribLocation(prg, 'position');
+attLocation[0] = gl.getAttribLocation(prg, 'color');
+var attStride = new Array(2);
+attStride[0] = 3;
+attStride[1] = 4;
+
+// Vertex posiion which will be bound to vbo 
+var vertex_position = [
+    // X,   Y,   Z
+     0.0, 1.0, 0.0,
+     1.0, 0.0, 0.0,
+    -1.0, 0.0, 0.0
+];
+
+var vertex_color = [
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0
+];
+
+// Create vbo
+var position_vbo = createVBO(vertex_position);
+gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
+gl.enableVertexAttribArray(attLocation[0]);
+gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
+
+var color_vbo = createVBO(vertex_color);
+gl.bindBuffer(gl.ARRAY_BUFFER, color_vbo);
+gl.enableVertexAttribArray(attLocation[1]);
+gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
+
+// Create mvpMatrix
+var m = new matIV;
+// 各種行列の生成と初期化
+var mMatrix = m.identity(m.create());   // matrix for model conversion
+var vMatrix = m.identity(m.create());   // matrix for view conversion
+var pMatrix = m.identity(m.create());   // matrix for projection conversion
+var mvpMatrix = m.identity(m.create()); // MVP matrix for uniform in VS
+
+m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+m.perspective(90, 300 / 300, 0.1, 100, pMatrix);
+
+// Convert MVP. p->v->m order 
+m.multiply(pMatrix, vMatrix, mvpMatrix);
+m.multiply(mvpMatrix, mMatrix, mvpMatrix);
+
+// Set mvp matrix to VS
+var uniformLocation = gl.getUniformLocation(prg, "mvpMatrix");
+gl.uniformMatrix4fv(uniformLocation, false, mvpMatrix);
+
+// Draw triangle
+gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.flush();
