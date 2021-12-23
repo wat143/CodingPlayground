e641f1e2ef0201242123665c9c518bbdddb895ed
Wataru Mizuno <watarumizuno1128@gmail.com>
2021-12-23 18:20:50 +0900

    WebGL: Phong shading
    Move lighting calculation from vertex shader to fragment shader.

--- WebGL/Torus.js
+++ WebGL/Torus.js
@@ -215,9 +215,8 @@ function matIV(){
 
 
 // Shader sources 
-vert = 'attribute vec3 position; attribute vec3 normal; attribute vec4 color; uniform mat4 mvpMatrix; uniform mat4 invMatrix; uniform vec3 lightDirection; uniform vec3 eyeDirection; uniform vec4 ambientLight; varying vec4 vColor; void main(void) { vec3 invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz; vec3 invEye = normalize(invMatrix * vec4(eyeDirection, 0.0)).xyz; vec3 halfLE = normalize(invLight + invEye); float diffuse = clamp(dot(normal, invLight), 0.0, 1.0); float specular = pow(clamp(dot(normal, halfLE), 0.0, 1.0), 50.0); vec4 light = color * vec4(vec3(diffuse), 1.0) + vec4(vec3(specular), 1.0); vColor = light + ambientLight; gl_Position = mvpMatrix * vec4(position, 1.0);}'
-frag = 'precision mediump float; varying vec4 vColor; void main() { gl_FragColor = vColor; }'
-
+vert = 'attribute vec3 position; attribute vec3 normal; attribute vec4 color; uniform mat4 mvpMatrix; varying vec3 vNormal; varying vec4 vColor; void main(){ vNormal = normal; vColor = color; gl_Position = mvpMatrix * vec4(position, 1.0);}'
+frag = 'precision mediump float; uniform mat4 invMatrix; uniform vec3 lightDirection; uniform vec3 eyeDirection; uniform vec4 ambientColor; varying vec3 vNormal; varying vec4 vColor; void main(){vec3 invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz; vec3  invEye = normalize(invMatrix * vec4(eyeDirection, 0.0)).xyz; vec3  halfLE = normalize(invLight + invEye); float diffuse = clamp(dot(vNormal, invLight), 0.0, 1.0); float specular = pow(clamp(dot(vNormal, halfLE), 0.0, 1.0), 50.0); vec4  destColor = vColor * vec4(vec3(diffuse), 1.0) + vec4(vec3(specular), 1.0) + ambientColor; gl_FragColor = destColor;}'
 // Create shader function 
 function createProgram(vs, fs) {
     var program = gl.createProgram();
