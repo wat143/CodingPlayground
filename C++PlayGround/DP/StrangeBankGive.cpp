9c18611d48a47358f40adc13d63c91f11ed2879c
Wataru Mizuno <watarumizuno1128@gmail.com>
2022-01-30 08:35:06 +0900

    DP practice: give and receive DP

---
+++ C++PlayGround/DP/StrangeBankGive.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int n, dp[100001];
+    vector<int> v;
+    cin >> n;
+    // Create input pattern
+    for (int i = 0, idx = 0, cnt = 1; i < n; i++, idx++) {
+        if (idx == 0) {
+            i = 1;
+            v.push_back(i);
+        }
+        else if (idx % 2 == 0) {
+            i = pow(6, cnt);
+            v.push_back(i);
+        }
+        else {
+            i = pow(9, cnt++);
+            v.push_back(i);
+        }
+    }
+    v.erase(prev(v.end()));
+    // Init DP table
+    for (int i = 0; i <= n; i++)
+        dp[i] = 100000;
+    dp[0] = 0;
+    // Calculation (Give DP)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < v.size(); j++) {
+            if (i + v[j] <= n)
+                dp[i + v[j]] = min(dp[i] + 1, dp[i + v[j]]);
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
