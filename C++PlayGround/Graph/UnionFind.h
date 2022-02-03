801c1a9186c3af0f91246b011a3f7cadaf13bc5f
Wataru Mizuno <watarumizuno1128@gmail.com>
2022-02-03 14:10:34 +0900

    Graph: create union find tree header

---
+++ C++PlayGround/Graph/UnionFind.h
@@ -0,0 +1,36 @@
+#ifndef __UNION_FIND_
+#define __UNION_FIND_
+
+#include<vector>
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> tree;
+    vector<int> rank;
+public:
+    UnionFind(int n): tree(vector<int>(n)), rank(vector<int>(n, 0)) {
+        for (int i = 0; i < n; i++) tree[i] = i;
+    };
+    int root(int x) {
+        if (tree[x] == x) return x;
+        else
+            return tree[x] = root(tree[x]);
+    }
+    bool sameRoot(int x, int y) {
+        return root(x) == root(y);
+    }
+    void unite(int x, int y) {
+        int root_x = root(x);
+        int root_y = root(y);
+        if (rank[root_y] > rank[root_x])
+            tree[root_x] = root_y;
+        else {
+            tree[root_y] = root_x;
+            if (rank[root_x] == rank[root_y])
+                rank[root_x]++;
+        }
+    }
+};
+
+#endif
