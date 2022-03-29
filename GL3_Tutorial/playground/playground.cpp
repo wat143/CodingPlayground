17a8ff3b88c64cb25a6594797902302dbb7cfa8e
Wataru Mizuno <watarumizuno1128@gmail.com>
2022-03-29 10:14:02 +0900

    GL3 tutorial: draw triangle with color

--- GL3_Tutorial/playground/playground.cpp
+++ GL3_Tutorial/playground/playground.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <map>
 #include <GL/glew.h>
 
 #include <glfw3.h>
 
 
 #include <glm/glm.hpp>
+#include "common/shader.hpp"
 using namespace glm;
+using namespace std;
 
 GLFWwindow* initGLFW_Window(int width, int height, char *window_name) {
 	// Initialise GLFW
@@ -51,18 +53,52 @@ bool initGLEW(GLFWwindow *window) {
 
 int main( void )
 {
+	GLfloat vertex[][3] = {{-1.0f, -1.0f, 0.0f},
+							{1.0f, -1.0f, 0.0f},
+							{0.0f, 1.0f, 0.0f}};
+	GLfloat color[][3] = {{1.0f, 0.0f, 0.0f},
+						{0.0f, 1.0f, 0.0f},
+						{0.0f, 0.0f, 1.0f}};
+	GLuint buff[2], program;
+	map<GLchar*, GLuint> attr;
+	GLchar *vert_modelspace = "vertexPosition_modelspace", *vertColor = "vertexColor";
 	GLFWwindow* window = initGLFW_Window(1024, 768, "Playground");
 	if (window == nullptr)
 		return -1;
 	if(!initGLEW(window))
 		return -1;
 
+	// Generate VBOs and bind
+	glGenBuffers(2, buff);
+	glBindBuffer(GL_ARRAY_BUFFER, buff[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, buff[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+	// Load shaders
+	program = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+	attr[vert_modelspace] = glGetAttribLocation(program, vert_modelspace);
+	attr[vertColor] = glGetAttribLocation(program, vertColor);
+
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-    do{
+	// Enable shader
+	glUseProgram(program);
+    // Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	do{
+		// Clear previous screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Draw nothing, see you in tutorial 2 !
-
+		glEnableVertexAttribArray(attr[vert_modelspace]);
+		glBindBuffer(GL_ARRAY_BUFFER, buff[0]);
+		glVertexAttribPointer(attr[vert_modelspace], 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glEnableVertexAttribArray(attr[vertColor]);
+		glBindBuffer(GL_ARRAY_BUFFER, buff[1]);
+		glVertexAttribPointer(attr[vertColor], 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Draw triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
         // Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
